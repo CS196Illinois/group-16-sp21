a4a617d80b0e19d4c53afedd0d793c79fdd38d59
afnanfd2 <vucirrha@gmail.com>
2021-03-09 16:34:45 -0600

    more messing around

--- Research/afnanfd2/react-app-test/src/index.js
+++ Research/afnanfd2/react-app-test/src/index.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import ReactDom from 'react-dom';
+import "./index.css";
 
-var foo = 5
-var bar = 1020
+const firstMod = {
+  name: 'Just Enough Items (JEI)',
+  author: 'mezz',
+  img: 'https://media.forgecdn.net/avatars/thumbnails/29/69/64/64/635838945588716414.jpeg'
+}
 
-function Greeting() {
-  return(
-    <div>
-      <ul>
-        <li>One</li>
-        <li>Secunder</li>
-      </ul>
-    </div>
+const secondMod = {
+  name: 'Quark',
+  author: 'Vazkii',
+  img: 'https://media.forgecdn.net/avatars/thumbnails/36/643/64/64/635941051938350675.png'
+}
+
+function ModList() {
+  return (
+    <section className='modlist'>
+      <Mod
+        name={firstMod.name}
+        author={firstMod.author}
+        img={firstMod.img}
+      />
+
+      <Mod
+        name={secondMod.name}
+        author={secondMod.author}
+        img={secondMod.img}
+      />
+    </section>
   );
 }
 
+const Mod = (props) => {
+  return(
+  <article className='mod'>
+    <img src={props.img} alt=''/>
+    <h2>{props.name}</h2>
+    <p> By </p>
+    <h4>{props.author}</h4>
+  </article>
+  );
+};
 
-// const Greeting = () => {
-//  return React.createElement('div', {}, "huh")
-//}
-
-ReactDom.render(<Greeting/>, document.getElementById("root"));
+ReactDom.render(<ModList/>, document.getElementById("root"));
