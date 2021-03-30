23fc96eda3c4bfc4d56e8020b8293a5ccec23a44
afnanfd2 <vucirrha@gmail.com>
2021-03-29 21:24:39 -0500

    Completed cataloguer for one item of choice

--- Research/afnanfd2/mysql-flask/flaskapp.py
+++ Research/afnanfd2/mysql-flask/flaskapp.py
@@ -1,16 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, send_file
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 import yaml
 
 app = Flask(__name__)
+CORS(app)
 
-# INSECURE configure database code
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'AzureFluke--44'
-# app.config['MYSQL_DB'] = 'flasktest'
-
-# SECURE code:
 dbInfo = yaml.load(open('db.yaml'), Loader=yaml.SafeLoader)
 app.config['MYSQL_HOST'] = dbInfo['mysql_host']
 app.config['MYSQL_USER'] = dbInfo['mysql_user']
@@ -18,18 +13,6 @@ app.config['MYSQL_PASSWORD'] = dbInfo['mysql_password']
 app.config['MYSQL_DB'] = dbInfo['mysql_db']
 
 mysql = MySQL(app)
-
-
-class Person:
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-
-    def to_dict(self):
-        return {
-            'name': self.name,
-            'age': self.age
-        }
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -56,32 +39,38 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/users')
-def users():
+# @app.route('/users')
+# def users():
+#     cursor = mysql.connection.cursor()
+#     cursor.execute("SELECT * FROM users")
+#     db = cursor.fetchall()
+
+@app.route('/image/<item_id>')
+def retrieve_image(item_id):
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM users")
+    cursor.execute("SELECT image FROM catalogue WHERE (item_id = {})".format(item_id))
+    db = cursor.fetchall()
+    # cursor.close()
+    return (
+        send_file("items/{}".format(db[0][0]))
+    )
+
+
+@app.route('/item/<item_id>')
+def retrieve_item(item_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("""
+        SELECT catalogue.item_name, categories.category_name, users.first_name
+        FROM catalogue
+            INNER JOIN categories ON catalogue.category_id = categories.category_id
+            INNER JOIN users ON catalogue.user_id = users.user_id
+        WHERE catalogue.item_id = {}
+    """.format(item_id))
     db = cursor.fetchall()
 
-    return render_template('users.html', userDetails=db)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-
-        cursor = mysql.connection.cursor()
-
-        # try:
-        cursor.execute(("SELECT * FROM users WHERE email = '{}'").format(email))
-        # except mysql.connection.IntegrityError:
-        #     return render_template("index.html", errorText="Email already exists in our database.")
-        # except mysql.connection.DataError:
-        #     return render_template("index.html", errorText="Email was too long")
-        user = cursor.fetchall()
-        return render_template('login.html', name=user[0][0])
-
-    return render_template('login.html')
+    return (
+        jsonify(db[0])
+    )
 
 
 if __name__ == '__main__':
